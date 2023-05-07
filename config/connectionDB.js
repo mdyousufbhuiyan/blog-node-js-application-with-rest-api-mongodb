7e40b40ed8b3ab254aa6491885e96f8649557c53
Md Yousuf Bhuiyan <mdyousufbhuiyan@Mds-MacBook-Pro.local>
2023-05-07 19:40:32 +0600

    added test route

--- config/connectionDB.js
+++ config/connectionDB.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        // await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(
+            'mongodb+srv://mdyousuf336:RUGTU0XzjKUxSGPX@cluster0.ozjnzdn.mongodb.net/blogRestApi?retryWrites=true&w=majority',
+        );
         console.log('Database connection success');
     } catch (error) {
         console.log(`Database connection failed ${error}`);
